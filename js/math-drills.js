b03f926d8578c03ef42b94d145563b33aee01da6
Shannan Kammann <sbkammann@gmail.com>
2018-11-05 18:10:35 -0800

    feat: feat: Progress bar

--- js/math-drills.js
+++ js/math-drills.js
@@ -1,18 +1,48 @@
-//returns random integer from 0 to 10
+//create an algorithm that reduces chance of duplicates and improces on the random numbers
+//create heat map for problem pairing
+
+
 let start = false;
 let historyStart = false;
 const child = document.getElementById('child');
 const history = document.getElementById('history');
+const progressBar = document.getElementById('progressBar');
+//get check box values
+const excludeZero = document.getElementById('excludeZero');
+const excludeOne = document.getElementById('excludeOne');
+excludeOne
 
 function randomNumber(){
+let num = 11;
+let add = 0;
 if (start) {
 history.insertBefore(child.childNodes[0] , history.childNodes[0]);
 }
 if (history.childNodes.length > 10) {
   history.removeChild(history.childNodes[10]);
 }
-let intOne = Math.floor(Math.random()*11); //change range later based on settings
-let intTwo = Math.floor(Math.random()*11); //change range later based on settings
+if (excludeZero.checked) { // can you have 0 but not 1?
+  add = 1;
+  num=10;
+}
+if (excludeOne.checked) {
+  add = 2;
+  num = 10;
+}
+else {
+  add = 0;
+  num = 11;
+}
+let intOne;
+if (document.getElementById('focusedMode').checked){
+  intOne = document.getElementById('focusNumber').value;
+}
+else {
+  intOne = Math.floor(Math.random()*num) + add;
+}
+
+let intTwo = Math.floor(Math.random()*num) + add;
+let progressBar = document.getElementById('focusNumber').value;
 
 let message = `${intOne} x ${intTwo} =`;
 const para = document.createElement('p');
@@ -28,6 +58,11 @@ function check(){
   if (result === parseInt(userInput)) {
     console.log("correct");
     child.childNodes[0].innerHTML += " "+result;
+    if (document.getElementById('limited').checked){
+      const progress = document.createElement('div');
+      progress.style.color = "green"; //.setAttribute('color', 'green'); need to set width etc. 
+      progressBar.appendChild(progress);
+    }
     randomNumber();
   }
   else {
